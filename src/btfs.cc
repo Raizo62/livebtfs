b795228c6dc4437405d0182e349538a761ca4292
raizo62 <david.ansart@cfautec.fr>
2018-04-23 19:47:59 +0200

    disable and comment access to DHT

--- src/btfs.cc
+++ src/btfs.cc
@@ -623,9 +623,11 @@ btfs_init(struct fuse_conn_info *conn) {
 	se.upload_rate_limit = params.max_upload_rate * 1024;
 
 	session->set_settings(se);
+/*
 	session->add_dht_router(std::make_pair("router.bittorrent.com", 6881));
 	session->add_dht_router(std::make_pair("router.utorrent.com", 6881));
 	session->add_dht_router(std::make_pair("dht.transmissionbt.com", 6881));
+*/
 	session->async_add_torrent(*p);
 #else
 	libtorrent::settings_pack pack;
@@ -646,12 +648,14 @@ btfs_init(struct fuse_conn_info *conn) {
 		STRINGIFY(LIBTORRENT_VERSION_MINOR)
 		"00";
 
+/*
 #if LIBTORRENT_VERSION_NUM >= 10101
 	pack.set_str(pack.dht_bootstrap_nodes,
 		"router.bittorrent.com:6881,"
 		"router.utorrent.com:6881,"
 		"dht.transmissionbt.com:6881");
 #endif
+*/
 
 	pack.set_int(pack.request_timeout, 10);
 	pack.set_str(pack.listen_interfaces, interfaces.str());
@@ -662,14 +666,17 @@ btfs_init(struct fuse_conn_info *conn) {
 	pack.set_int(pack.upload_rate_limit, params.max_upload_rate * 1024);
 	pack.set_int(pack.alert_mask, alerts);
 	pack.set_int(pack.seed_choking_algorithm, libtorrent::settings_pack::fastest_upload);
+	pack.set_bool(libtorrent::settings_pack::enable_dht, false);
 
 	session = new libtorrent::session(pack, flags);
 
+/*
 #if LIBTORRENT_VERSION_NUM < 10101
 	session->add_dht_router(std::make_pair("router.bittorrent.com", 6881));
 	session->add_dht_router(std::make_pair("router.utorrent.com", 6881));
 	session->add_dht_router(std::make_pair("dht.transmissionbt.com", 6881));
 #endif
+*/
 
 	session->add_torrent(*p);
 #endif
