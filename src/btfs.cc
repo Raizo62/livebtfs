a6037936767d2876a837004f8543052c06339140
raizo62 <david.ansart@cfautec.fr>
2018-04-24 19:23:11 +0200

    disable DHT with syntax more coherent with others options

--- src/btfs.cc
+++ src/btfs.cc
@@ -669,7 +669,7 @@ btfs_init(struct fuse_conn_info *conn) {
 	pack.set_int(pack.alert_mask, alerts);
 	pack.set_int(pack.seed_choking_algorithm, libtorrent::settings_pack::fastest_upload);
 	pack.set_bool(pack.prioritize_partial_pieces, true);
-	pack.set_bool(libtorrent::settings_pack::enable_dht, false);
+	pack.set_bool(pack.enable_dht, false);
 
 	session = new libtorrent::session(pack, flags);
 
