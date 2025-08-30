b0efdb72b28c142acc67d4f94875302ccb5929c3
Raizo62 <silicium62-github@yahoo.fr>
2025-08-30 10:18:34 +0200

    livebtfs : explain why "FUSE_USE_VERSION" must be a "#define" and not a "const"

--- src/livebtfs.cc
+++ src/livebtfs.cc
@@ -17,6 +17,7 @@ You should have received a copy of the GNU General Public License
 along with BTFS.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+// FUSE waits a macro #define
 #define FUSE_USE_VERSION 31
 
 //#define _DEBUG
